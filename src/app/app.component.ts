b58245afc616d34d90cbd022a97eb6345f0537a2
dowonkim <dowoniscool@yahoo.com>
2024-06-12 17:22:19 +0200

    chore : Exercise : Create a Configurable Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
+import {TasksComponent} from "./tasks/tasks.component";
 import { DUMMY_USERS } from "./dummy-users";
 
 @Component({
@@ -8,14 +9,22 @@ import { DUMMY_USERS } from "./dummy-users";
   standalone: true,
   imports: [
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    TasksComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  selectedUserId = "u1"
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
   onSelectedUser(id: string) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
